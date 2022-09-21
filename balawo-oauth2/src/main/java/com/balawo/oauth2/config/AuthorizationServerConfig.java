50f27b1d322029446ad5610f4e29b2d05971271d
yanchengv <zqalyc@163.com>
2022-09-21 17:49:57 +0800

    auth2 获取token方法

--- balawo-oauth2/src/main/java/com/balawo/oauth2/config/AuthorizationServerConfig.java
+++ balawo-oauth2/src/main/java/com/balawo/oauth2/config/AuthorizationServerConfig.java
@@ -1,6 +1,8 @@
 package com.balawo.oauth2.config;
 
+import com.balawo.oauth2.service.MyClientDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -8,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
@@ -28,6 +31,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private TokenStore tokenStore;
 
+    @Bean
+    public ClientDetailsService clientDetailsServiceBean(){
+        MyClientDetailsService myClientDetailsService = new MyClientDetailsService();
+        return myClientDetailsService;
+    }
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
@@ -36,19 +45,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                //配置client_id
-                .withClient("adminc")
-                //配置client-secret
-                .secret(passwordEncoder.encode("123123"))
-                //配置访问token的有效期
-                .accessTokenValiditySeconds(3600)
-                .redirectUris("http://www.baidu.com")
-                //配置申请的权限范围
-                .scopes("all")
-                //配置grant_type 表示授权模式 授权码模式获取code: localhost:9003/oauth/authorize?response_type=code&client_id=adminc&redirect_uri=http://www.baidu.com&scope=all
-                //.authorizedGrantTypes("authorization_code");
-                .authorizedGrantTypes("password");
+        clients.withClientDetails(clientDetailsServiceBean());
 
     }
 }
